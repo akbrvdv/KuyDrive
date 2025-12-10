07c10a15dbb2a464d9bf94ca904ee7999e7f45b9
akbrvdv <muhakbarf17@gmail.com>
2025-12-11 00:11:20 +0700

    fix

--- app/src/main/java/com/kelompok6/kuydrive/LoginActivity.kt
+++ app/src/main/java/com/kelompok6/kuydrive/LoginActivity.kt
@@ -12,14 +12,13 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login) // Pastikan nama layout sesuai
+        setContentView(R.layout.activity_login)
 
         val etEmail = findViewById<EditText>(R.id.etEmail)
         val etPassword = findViewById<EditText>(R.id.etPassword)
         val btnLogin = findViewById<Button>(R.id.btnLogin)
         val tvRegister = findViewById<TextView>(R.id.tvRegisterLink)
 
-        // Pindah ke Halaman Register
         tvRegister.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
@@ -34,14 +33,12 @@ class LoginActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            // Logika Login Sederhana
             if (emailInput == "admin@gmail.com" && passwordInput == "12345") {
                 Toast.makeText(this, "Login Berhasil! Halo Admin.", Toast.LENGTH_SHORT).show()
 
-                // Pindah ke MainActivity
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
-                finish() // Menutup LoginActivity agar tidak bisa kembali saat ditekan Back
+                finish()
             } else {
                 Toast.makeText(this, "Email atau Password Salah!", Toast.LENGTH_SHORT).show()
             }
