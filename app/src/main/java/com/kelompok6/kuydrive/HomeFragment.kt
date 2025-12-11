dff2e5cf36517e446003a2cce18241b7c57cb89c
akbrvdv <muhakbarf17@gmail.com>
2025-12-11 22:59:31 +0700

    fix home page

--- app/src/main/java/com/kelompok6/kuydrive/HomeFragment.kt
+++ app/src/main/java/com/kelompok6/kuydrive/HomeFragment.kt
@@ -5,10 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
-import androidx.cardview.widget.CardView
+import androidx.cardview.widget.CardView // Pastikan import ini ada
 import androidx.fragment.app.Fragment
 
 class HomeFragment : Fragment() {
@@ -23,15 +22,19 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<LinearLayout>(R.id.btnSearch)?.setOnClickListener {
-            openMap()
-        }
+        // Klik Tombol Utama "Pesan Ojek"
         view.findViewById<CardView>(R.id.btnKuyRide)?.setOnClickListener {
             openMap()
         }
 
+        // Klik Search Bar (Juga membuka map)
+        view.findViewById<CardView>(R.id.btnSearch)?.setOnClickListener {
+            openMap()
+        }
+
+        // Klik Voucher
         view.findViewById<TextView>(R.id.btnVoucher1)?.setOnClickListener {
-            Toast.makeText(context, "Voucher Berhasil Diklaim!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, "Voucher Diskon Berhasil Diklaim!", Toast.LENGTH_SHORT).show()
         }
     }
 
