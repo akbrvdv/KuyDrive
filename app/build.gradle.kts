d6244a6ad84c1ec9878586d0ed7b1dc1c37b328e
akbrvdv <muhakbarf17@gmail.com>
2025-12-11 21:41:35 +0700

    fix tema app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation("org.osmdroid:osmdroid-android:6.1.16")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
